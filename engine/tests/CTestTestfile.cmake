2b01f380a9ddc0a09f246590b977de98ad22dd81
Patrick M. Niedzielski <PatrickNiedzielski@gmail.com>
2010-02-28 04:26:44 +0000

    Accidentally added too many files in the tests directory.

--- engine/tests/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/pniedzielski/Projects/hummstrumm/src/engine/tests
-# Build directory: /home/pniedzielski/Projects/hummstrumm/src/engine/tests
-# 
-# This file includes the relevent testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-ADD_TEST(test_engine_log "/home/pniedzielski/Projects/hummstrumm/src/engine/tests/test_engine_log")
