c32c228057bd70105e4fced745710d70a095c633
Patrick M. Niedzielski <PatrickNiedzielski@gmail.com>
2010-06-19 15:25:23 +0000

    I must have forgot to add the files to the subversion repo.  Let's try this.

---
+++ engine/include/error/unicode.hpp
@@ -0,0 +1,78 @@
+// -*- c++ -*-
+/* Humm and Strumm Video Game
+ * Copyright (C) 2008-2010, the people listed in the AUTHORS file. 
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * Defines the Unicode exception class.
+ *
+ * @file   unicode.hpp
+ * @author Patrick Michael Niedzielski <PatrickNiedzielski@gmail.com>
+ * @date   2010-01-03
+ * @see    Unicode
+ */
+
+#ifndef HUMMSTRUMM_ENGINE_ERROR_UNICODE
+#define HUMMSTRUMM_ENGINE_ERROR_UNICODE
+
+#include <string>
+#include "error.hpp"
+
+namespace hummstrumm
+{
+namespace engine
+{
+namespace error
+{
+
+/**
+ * The error for generic Unicode problems...there are a lot of them... (>_<*)
+ *
+ * @author Patrick M. Niedzielski <PatrickNiedzielski@gmail.com>
+ * @date 2010-06-08
+ * @since 0.3
+ */
+class Unicode : public Error
+{
+  public:
+    /**
+     * Constructs a Unicode object to be thrown to an error handler.
+     *
+     * @author Patrick M. Niedzielski <PatrickNiedzielski@gmail.com>
+     * @date 2010-06-08
+     * @since 0.3
+     *
+     * @param fileName [in] The name of the file in which the error occured.
+     * @param lineNumber [in] The line of the file in which the error occured.
+     * @param text [in] A human readable description of the error.
+     */
+    Unicode (const char *fileName, unsigned int lineNumber,
+             const char *text = "");
+    /**
+     * Destructs a Unicode object.
+     *
+     * @author Patrick M. Niedzielski <PatrickNiedzielski@gmail.com>
+     * @date 2010-06-08
+     * @since 0.3
+     */
+    virtual ~Unicode (void);
+};
+
+}
+}
+}
+
+#endif // #ifndef HUMMSTRUMM_ENGINE_ERROR_UNICODE
---
+++ engine/src/types/inttypes.cpp
@@ -0,0 +1,40 @@
+// -*- c++ -*-
+/* Humm and Strumm Video Game
+ * Copyright (C) 2008-2010, the people listed in the AUTHORS file. 
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#define HUMMSTRUMM_ENGINE_SOURCE
+#include "hummstrummengine.hpp"
+
+namespace hummstrumm
+{
+namespace engine
+{
+namespace types
+{
+
+const int16 INT16_MIN  (-32768);
+const int16 INT16_MAX  (+32767);
+const uint16 UINT16_MAX (+65535u);
+const int32 INT32_MIN  (-2147483648l);
+const int32 INT32_MAX  (+2147483647l);
+const uint32 UINT32_MAX (+4294967295ul);
+const int64 INT64_MIN  (-9223372036854775808ll);
+const int64 INT64_MAX  (+9223372036854775807ll);
+const uint64 UINT64_MAX (+18446744073709551615ull);
+
+}
+}
+}
