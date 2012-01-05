a0d377377dee6cdc6005b13ac90e0bfce6b585ad
Patrick M. Niedzielski <PatrickNiedzielski@gmail.com>
2012-01-05 17:45:12 +0000

    Change copyright date ranges for the project.

--- cmake/Uninstall.cmake
+++ cmake/Uninstall.cmake
@@ -1,5 +1,5 @@
 # Humm and Strumm Video Game
-# Copyright (C) 2008-2011, the people listed in the AUTHORS file. 
+# Copyright (C) 2008-2012, the people listed in the AUTHORS file. 
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
