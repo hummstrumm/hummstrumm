62afaf081e3d26c630d73a141142f5e7f32388fb
Patrick M. Niedzielski <PatrickNiedzielski@gmail.com>
2012-08-08 00:14:01 +0000

    Change the VariableLogging.cmake file to show prettier output.

--- cmake/VariableLogging.cmake
+++ cmake/VariableLogging.cmake
@@ -17,46 +17,48 @@
 # VariableLogging.cmake -- Outputs the build configuration.
 
 # Print a nice header.
-message (STATUS "")
-message (STATUS "${HUMMSTRUMM_ENGINE_NAME} ${HUMMSTRUMM_ENGINE_VERSION} ")
-message (STATUS "====================================================================")
-message (STATUS "  ${HUMMSTRUMM_ENGINE_DESCRIPTION}")
-message (STATUS "  ${HUMMSTRUMM_ENGINE_VENDOR} <${HUMMSTRUMM_ENGINE_WEBSITE}>")
-message (STATUS "")
-message (STATUS "In ${CMAKE_BUILD_TYPE} mode")
-message (STATUS "Building with ${CMAKE_CXX_COMPILER_ID} compiler for ${CMAKE_SYSTEM_NAME} ${CMAKE_SYSTEM_PROCESSOR}")
-message (STATUS "")
-message (STATUS "Features enabled:")
+message ("")
+message ("====================================================================")
+message ("${HUMMSTRUMM_ENGINE_NAME} ${HUMMSTRUMM_ENGINE_VERSION} ")
+message ("====================================================================")
+message ("  ${HUMMSTRUMM_ENGINE_DESCRIPTION}")
+message ("  ${HUMMSTRUMM_ENGINE_VENDOR} <${HUMMSTRUMM_ENGINE_WEBSITE}>")
+message ("")
+message ("In ${CMAKE_BUILD_TYPE} mode")
+message ("Building with ${CMAKE_CXX_COMPILER_ID} compiler for ${CMAKE_SYSTEM_NAME} ${CMAKE_SYSTEM_PROCESSOR}")
+message ("")
+message ("Features enabled:")
 
 # Are we targetting X11?
 if (HUMMSTRUMM_ENGINE_WINDOWSYSTEM_X11)
-  message (STATUS "  * X11 windowing code")
+  message ("  * X11 windowing code")
 endif ()
 
 # Are we targetting WinAPI?
 if (HUMMSTRUMM_ENGINE_WINDOWSYSTEM_WINDOWS)
-  message (STATUS "  * Win native windowing code")
+  message ("  * Win native windowing code")
 endif ()
 
 # Are we building unit tests?
 if (IS_BUILDING_UNIT_TESTS)
-  message (STATUS "  * Unit tests")
+  message ("  * Unit tests")
 endif ()
 
 # Are we generating documentation?
 if (HUMMSTRUMM_ENGINE_BUILD_DOCS)
-  message (STATUS "  * HTML documentation")
+  message ("  * HTML documentation")
 endif ()
 
 # Do we have SSE support?
 if (HUMMSTRUMM_ENGINE_HAVE_SSE_SUPPORT)
-  message (STATUS "  * SSE support")
+  message ("  * SSE support")
 endif ()
 
-message (STATUS "")
-message (STATUS "This is the ${HUMMSTRUMM_ENGINE_VERSION} release of ${HUMMSTRUMM_ENGINE_NAME}.")
-message (STATUS "For a list of changes in this release, see the packaged NEWS file.")
-message (STATUS "For help with installation, see the packaged INSTALL file.")
-message (STATUS "For your legal rights, see the packaged COPYING file.")
-message (STATUS "For more information, see the packaged README file.")
-message (STATUS "")
+message ("")
+message ("This is the ${HUMMSTRUMM_ENGINE_VERSION} release of ${HUMMSTRUMM_ENGINE_NAME}.")
+message ("For a list of changes in this release, see the packaged NEWS file.")
+message ("For help with installation, see the packaged INSTALL file.")
+message ("For your legal rights, see the packaged COPYING file.")
+message ("For more information, see the packaged README file.")
+message ("====================================================================")
+message ("")
