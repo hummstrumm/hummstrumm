96afc621b4e4ad41831a17987b4f15829a953b09
Patrick M. Niedzielski <PatrickNiedzielski@gmail.com>
2014-02-16 01:34:16 +0000

    Move SSE checks from the main CMakeLists.txt to the CompilerChecks.cmake file.

--- cmake/CompilerChecks.cmake
+++ cmake/CompilerChecks.cmake
@@ -23,3 +23,27 @@ include (CheckCpp11)
 check_include_file_cxx (cpuid.h HAVE_CPUID_H)
 check_include_file_cxx (unistd.h HAVE_UNISTD_H)
 check_include_file_cxx (memory HAVE_MEMORY)
+
+# Test for SIMD
+set (HUMMSTRUMM_ENGINE_USE_SSE ON CACHE BOOL
+     "Whether to build the engine with SSE support or not.")
+set (HUMMSTRUMM_ENGINE_HAVE_SSE_SUPPORT OFF)
+
+if (HUMMSTRUMM_ENGINE_USE_SSE)
+  try_run (RUN_RESULT_VAR COMPILE_RESULT_VAR
+           ${CMAKE_BINARY_DIR}
+           ${CMAKE_CURRENT_SOURCE_DIR}/cmake/system/detectSSE.c
+           CMAKE_FLAGS
+           COMPILE_DEFINITIONS ${HUMMSTRUMM_ENGINE_REQUIRED_DEFINITIONS}
+           COMPILE_OUTPUT_VARIABLE COMPILE_OUTPUT_VAR)
+  #message (STATUS ${COMPILE_OUTPUT_VAR})
+  if (${RUN_RESULT_VAR})
+    set (HUMMSTRUMM_ENGINE_HAVE_SSE_SUPPORT ON)
+    set (HUMMSTRUMM_ENGINE_DOXYGEN_MACROS "${HUMMSTRUMM_ENGINE_DOXYGEN_MACROS} HUMMSTRUMM_ENGINE_HAVE_SSE_SUPPORT=")
+    if (HUMMSTRUMM_ENGINE_COMPILER_CLANG OR HUMMSTRUMM_ENGINE_COMPILER_GCC)
+      add_definitions("-msse")
+#      add_definitions("-msse4.1")
+#      add_definitions("-msse4.2")
+    endif ()
+  endif (${RUN_RESULT_VAR})
+endif (HUMMSTRUMM_ENGINE_USE_SSE)
